ffe5554f1c6de298843158fa9a3d209705032aca
Xavier Stouder <xavier@stouder.io>
2018-04-18 20:00:57 +0200

    Fix

--- src/routes/save-order.js
+++ src/routes/save-order.js
@@ -2,14 +2,11 @@ const path = require('path')
 const archiver = require('archiver')
 const globby = require('globby')
 
-
 const fs = require('fs')
 
 module.exports = async function(req, res) {
-
   res.writeHead(200, {
-    'Content-Type': 'application/octet-stream',
-    'Transfer-Encoding': 'chunked'
+    'Content-Type': 'application/octet-stream'
   })
 
   const archive = archiver('zip', {
@@ -20,8 +17,9 @@ module.exports = async function(req, res) {
   res.on('end', () => console.log('Send end'))
   archive.on('error', err => console.log(err))
   archive.on('end', () => console.log('Archived'))
+  const debugStream = require('debug-stream')('my-app')
 
-  archive.pipe(res)
+  archive.pipe(debugStream()).pipe(res)
 
   const filenames = await globby(req.body.includes, { ignore: req.body.excludes })
   filenames.forEach(filename => archive.file(filename, {
